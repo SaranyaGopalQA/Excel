4d1788567316d8f0e6449517be846b44c585539b
name <email>
2022-08-11 14:34:33 +0530

    Inital commit

---
+++ src/Write.java
@@ -0,0 +1,68 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+
+public class Write {
+    FileInputStream fis;
+    XSSFWorkbook workbook;
+    XSSFSheet sheet;
+    XSSFRow row;
+    XSSFCell cell;
+    FileOutputStream fileOut;
+    String path="\\Desktop\\writeexcel.xlsx";
+    public boolean setCellData(String sheetName, String colName, int rowNum, String data) {
+        try {
+            fis = new FileInputStream(path);
+            workbook = new XSSFWorkbook(fis);
+
+            if (rowNum <= 0)
+                return false;
+
+            int index = workbook.getSheetIndex(sheetName);
+            int colNum = -1;
+            if (index == -1)
+                return false;
+
+            sheet = workbook.getSheetAt(index);
+
+            row = sheet.getRow(0);
+            for (int i = 0; i < row.getLastCellNum(); i++) {
+                // System.out.println(row.getCell(i).getStringCellValue().trim());
+                if (row.getCell(i).getStringCellValue().trim().equals(colName))
+                    colNum = i;
+            }
+            if (colNum == -1)
+                return false;
+
+            sheet.autoSizeColumn(colNum);
+            row = sheet.getRow(rowNum - 1);
+            if (row == null)
+                row = sheet.createRow(rowNum - 1);
+
+            cell = row.getCell(colNum);
+            if (cell == null)
+                cell = row.createCell(colNum);
+
+            cell.setCellValue(data);
+
+            fileOut = new FileOutputStream(path);
+
+            workbook.write(fileOut);
+
+            fileOut.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Write w=new Write();
+        w.setCellData("sheet1","A",1,"Testing");
+
+    }
+}
+
+
