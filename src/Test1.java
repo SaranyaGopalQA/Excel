eebff28cb8e7a2ceb99a7d18fbffd386df7478a3
name <email>
2022-08-16 10:29:35 +0530

    Added Test1

---
+++ src/Test1.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+
+import java.io.FileReader;
+import java.util.Properties;
+
+public class Test1 {
+
+        public static WebDriver driver;
+        public static Properties p = new Properties();
+        public static Properties v = new Properties();
+        public static FileReader r;
+        public static FileReader r1;
+
+    }
+
